c32c7b3160dedac88a1218d6b3ab88104e5826b1
abdelmoghit idhsaine <idhsaine.abdelmoghit@inemail.ine.inpt.ma>
2022-03-22 12:39:38 +0100

    api shop with typescript

--- server.tsx
+++ server.tsx
@@ -2,6 +2,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
+const items = require("./routes/api/items.tsx")
+
 //Initialiser "express" dans une variable nommée app
 const app = express()
 
@@ -15,6 +17,8 @@ const db = require("./config/keys").mongoURI
 mongoose.connect(db)
     .then(()=> console.log("Mongo connected ..."))
     .catch(err=>console.log(err))
+
+app.use("/api/items", items);
 
 const port = process.env.PORT || 6000
 
